c3097afe334fc84b59c2c120ba126377518b3858
akira ogawa <oakira@outlook.com>
2020-02-22 22:42:33 +0900

    add pros01

--- processing/01/sketch.js
+++ processing/01/sketch.js
@@ -1,6 +1,33 @@
+let x;
+let y;
+let w;
 function setup() {
-
+	createCanvas(640, 360);
+	background(255);
+	w = new Walker();
 }
 function draw() {
-	ellipse(50, 50, 80, 80);
+	frameRate(30);
+	w.step();
+	w.display();
+}
+class Walker {
+	constructor() {
+		this.Walker();
+	}
+	Walker() {
+		x = width / 2;
+		y = height / 2;
+	}
+	display() {
+		stroke(0);
+		point(x, y);
+	}
+	step() {
+		const randRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+		let stepx = randRange(-1, 1);
+		let stepy = randRange(-1, 1);
+		x += stepx;
+		y += stepy;
+	}
 }
